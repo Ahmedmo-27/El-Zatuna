e0bb6048494b2df4a29500e2b7b4c21c425d9d76
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2026-05-22 00:49:03 +0300

    Refactor migration to conditionally create and modify live session activity logs table

--- database/migrations/2026_05_20_215820_create_live_session_activity_logs_table.php
+++ database/migrations/2026_05_20_215820_create_live_session_activity_logs_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('live_session_activity_logs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('live_session_activity_logs')) {
+            Schema::create('live_session_activity_logs', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
