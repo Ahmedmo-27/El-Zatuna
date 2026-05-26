3341bdbd17fc93de94d5d87496517b0d1e8e03b0
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2026-05-26 19:02:33 +0300

    Remove unused Queueable trait and ShouldQueue interface from VerifyRegistrationEmailCode notification

--- app/Notifications/VerifyRegistrationEmailCode.php
+++ app/Notifications/VerifyRegistrationEmailCode.php
@@ -2,15 +2,11 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class VerifyRegistrationEmailCode extends Notification implements ShouldQueue
+class VerifyRegistrationEmailCode extends Notification
 {
-    use Queueable;
-
     private $verificationCode;
     private $expiresAt;
     private $verificationToken;
